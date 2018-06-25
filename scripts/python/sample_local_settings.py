cd77a44d4a39c07f69c6dc1b63f36d5b7f943b98
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-06-25 22:02:24 +0600

    Added sample local settings file and updated requirements packages.

---
+++ scripts/python/sample_local_settings.py
@@ -0,0 +1,30 @@
+import sys
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_djaein',
+        'USER': 'djaein',
+        'PASSWORD': 'djaein',
+        'HOST': 'localhost',
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'djaein',
+            'USER': 'djaein',
+            'PASSWORD': 'djaein',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=',
+    '--cover-html',
+    '--cover-erase'
+]
+
+ALLOWED_HOSTS = ['*']
