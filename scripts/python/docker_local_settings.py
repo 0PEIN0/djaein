0f64c09a3e4b5a08f17789343064f7c59e39a1e6
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-06-26 05:24:09 +0600

    Updated docker config.

---
+++ scripts/python/docker_local_settings.py
@@ -0,0 +1,30 @@
+import sys
+
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_djaein',
+        'USER': 'djaein',
+        'PASSWORD': 'djaein',
+        'HOST': 'db',
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'djaein',
+            'USER': 'djaein',
+            'PASSWORD': 'djaein',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
+    }
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=users,personal',
+    '--cover-html',
+    '--cover-erase'
+]
+
+ALLOWED_HOSTS = ['*']
