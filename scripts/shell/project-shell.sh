5f49162e59bd39d8359ac7f170e32ddb00e66afd
S. M. Ijaz-ul-Amin Chowdhury <cetazo@gmail.com>
2018-06-25 20:44:58 +0600

    Updated project shell script.

--- scripts/shell/project-shell.sh
+++ scripts/shell/project-shell.sh
@@ -25,66 +25,6 @@ DJAEIN_INIT_POSTGRES_DB_FILE_PATH="$SYSTEM_ROOT_GIT_REPO_FOLDER/$DJAEIN_REPO_NAM
 DJAEIN_INIT_POSTGRES_DB_USER_PASSWORD_FILE_PATH="$SYSTEM_ROOT_GIT_REPO_FOLDER/$DJAEIN_REPO_NAME/scripts/sql/$DJAEIN_POSTGRES_USER_PASSWORD_RESET_FILE_NAME"
 DJAEIN_INIT_DATA_LOAD_PYTHON_FILE_PATH="$SYSTEM_ROOT_GIT_REPO_FOLDER/$DJAEIN_REPO_NAME/scripts/python/$DJAEIN_INIT_DATA_LOAD_PYTHON_FILE_NAME"
 
-if [ -f $BASH_HELPER_GIT_FOLDER/src/bash/django.sh ]; then
-    . $BASH_HELPER_GIT_FOLDER/src/bash/django.sh
-fi;
-
-djaeinDjangoDefaultSetup() {
-  if [ -z "$1" ]; then
-    echo 'null value not allowed as first parameter! You must pass the required parameter(s).'
-    return $1
-  fi;
-  if [ -z "$2" ]; then
-    echo 'null value not allowed as second parameter! You must pass the required parameter(s).'
-    return $2
-  fi;
-  if [ -z "$3" ]; then
-    echo 'null value not allowed as third parameter! You must pass the required parameter(s).'
-    return $3
-  fi;
-  cd $1/
-  eval ${3}_ve
-  eval "pip install -r $1/$2"
-  eval ${3}_init_data_load
-  eval ${3}_dir
-}
-
-djaeinDjangoRun() {
-  if [ -z "$1" ]; then
-    echo 'null value not allowed as first parameter! You must pass the required parameter(s).'
-    return $1
-  fi;
-  if [ -z "$2" ]; then
-    echo 'null value not allowed as second parameter! You must pass the required parameter(s).'
-    return $2
-  fi;
-  if [ -z "$3" ]; then
-    echo 'null value not allowed as third parameter! You must pass the required parameter(s).'
-    return $3
-  fi;
-  source ~/.zshrc
-  eval ${1}_ve
-  eval ${1}_stop_processes
-  eval ${1}_start_celery
-  ./manage.py runserver 0.0.0.0:${3}
-  echo "$2 Project Server Running!"
-}
-
-djaeinGitRebase() {
-  if [ -z "$1" ]; then
-    echo 'null value not allowed as first parameter! You must pass the required parameter(s).'
-    return $1
-  fi;
-  if [ -z "$2" ]; then
-    echo 'null value not allowed as second parameter! You must pass the required parameter(s).'
-    return $2
-  fi;
-  eval ${1}_ve
-  eval "git_r $2"
-  ./manage.py migrate
-  eval ${1}_init_data_load
-}
-
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_api_spec="${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve && ./manage.py api_spec"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc="djangoBranchChange $DJAEIN_PROJECT_NAME_UNDERSCORE "
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset="djangoBranchChangeWithFullReset $DJAEIN_PROJECT_NAME_UNDERSCORE "
@@ -92,6 +32,9 @@ alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset_and_run="djangoBranchChang
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcd="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc develop"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcd_full_reset="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset develop"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcd_full_reset_and_run="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset_and_run develop"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcm="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc master"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcm_full_reset="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset master"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcm_full_reset_and_run="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bc_full_reset_and_run master"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcd_run="${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcd && ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_bcr="djangoBranchChangeRun $DJAEIN_PROJECT_NAME_UNDERSCORE "
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_db_shell="${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve && ./manage.py shell"
@@ -100,8 +43,9 @@ alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_frr="${DJAEIN_PROJECT_NAME_UNDERSCORE}_f
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_full_reset="djangoFullReset $DJAEIN_PROJECT_NAME_UNDERSCORE"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_gf="${DJAEIN_PROJECT_NAME_UNDERSCORE}_dir && git_f"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_setup="djangoGitSetup $DJAEIN_GIT_REPO"
-alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_r="djaeinGitRebase $DJAEIN_PROJECT_NAME_UNDERSCORE "
-alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_rd="djaeinGitRebase $DJAEIN_PROJECT_NAME_UNDERSCORE develop"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_r="djangoGitRebaseWithDataLoad $DJAEIN_PROJECT_NAME_UNDERSCORE "
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_rd="djangoGitRebaseWithDataLoad $DJAEIN_PROJECT_NAME_UNDERSCORE develop"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_git_rm="djangoGitRebaseWithDataLoad $DJAEIN_PROJECT_NAME_UNDERSCORE master"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_init_data_load="${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve && ./manage.py migrate && ./manage.py shell < $DJAEIN_INIT_DATA_LOAD_PYTHON_FILE_PATH"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_installs="${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_init && cd \"$SYSTEM_ROOT_GIT_REPO_FOLDER\" && git clone ${DJAEIN_GIT_REPO} && ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_installs && ${DJAEIN_PROJECT_NAME_UNDERSCORE}_dir"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_last_10_commit_hash="${DJAEIN_PROJECT_NAME_UNDERSCORE}_dir && git log -10 --pretty=format:\"%h\""
@@ -116,7 +60,7 @@ alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_psql_reset="djangoPsqlReset $DJAEIN_POST
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_reset="djangoReset $DJAEIN_PROJECT_NAME_UNDERSCORE"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_rr="${DJAEIN_PROJECT_NAME_UNDERSCORE}_reset && ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_rt="${DJAEIN_PROJECT_NAME_UNDERSCORE}_reset && ${DJAEIN_PROJECT_NAME_UNDERSCORE}_test"
-alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run="djaeinDjangoRun $DJAEIN_PROJECT_NAME_UNDERSCORE $DJAEIN_PROJECT_NAME_UNDERSCORE $DJAEIN_LOCAL_PORT"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run="djangoRun $DJAEIN_PROJECT_NAME_UNDERSCORE $DJAEIN_PROJECT_NAME_UNDERSCORE $DJAEIN_LOCAL_PORT"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run_production="djangoProductionRun $DJAEIN_PROJECT_NAME_UNDERSCORE $DJAEIN_PROJECT_NAME $DJAEIN_LOCAL_PORT"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_run_generic_deployment="djangoDeploymentOperations $DJAEIN_PROJECT_NAME_UNDERSCORE"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_start_celery="startCeleryWorkers $DJAEIN_CELERY_PROJECT_NAME $DJAEIN_CELERY_FLOWER_HOST $DJAEIN_CELERY_FLOWER_PORT"
@@ -126,4 +70,4 @@ alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve="djangoUseVe \"$DJAEIN_VIRTUAL_ENVIRO
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_check="checkVirtualPythonEnvironmentFolder \"$DJAEIN_VIRTUAL_ENVIRONMENT_FOLDER\" \"$SYSTEM_ROOT_VIRTUAL_PYTHON_ENVIRONMENT_FOLDER\" $DJAEIN_VIRTUAL_ENVIRONMENT_NAME"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_clear="djangoVeClear \"$SYSTEM_ROOT_VIRTUAL_PYTHON_ENVIRONMENT_FOLDER\" $DJAEIN_VIRTUAL_ENVIRONMENT_NAME"
 alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_init="pythonVeVersionDecider \"$DJAEIN_VIRTUAL_ENVIRONMENT_FOLDER\" \"$SYSTEM_ROOT_VIRTUAL_PYTHON_ENVIRONMENT_FOLDER\" $DJAEIN_VIRTUAL_ENVIRONMENT_NAME $DJAEIN_PROJECT_NAME \"$DJAEIN_PYTHON_VERSION\""
-alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_installs="djaeinDjangoDefaultSetup \"$DJAEIN_PROJECT_ROOT_FOLDER\" $DJAEIN_INIT_REQUIREMENTS_FILE_NAME $DJAEIN_PROJECT_NAME_UNDERSCORE"
+alias ${DJAEIN_PROJECT_NAME_UNDERSCORE}_ve_installs="djangoDefaultSetupWithDataLoad \"$DJAEIN_PROJECT_ROOT_FOLDER\" $DJAEIN_INIT_REQUIREMENTS_FILE_NAME $DJAEIN_PROJECT_NAME_UNDERSCORE"
